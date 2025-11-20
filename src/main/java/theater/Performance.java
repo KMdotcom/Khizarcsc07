40b4bbe7e3baa0ae1aef15cdadfe3b34eb5e38a6
KMdotcom <khizarmaraj01@gmail.com>
2025-11-20 01:03:18 -0500

    performance.java

--- src/main/java/theater/Performance.java
+++ src/main/java/theater/Performance.java
@@ -1,15 +1,23 @@
 package theater;
 
 /**
- * Class representing a performance of a play..
+ * Represents a performance in an invoice.
  */
 public class Performance {
 
-    public String playID;
-    public int audience;
+    private String playID;
+    private int audience;
 
     public Performance(String playID, int audience) {
         this.playID = playID;
         this.audience = audience;
+    }
+
+    public String getPlayID() {
+        return playID;
+    }
+
+    public int getAudience() {
+        return audience;
     }
 }
